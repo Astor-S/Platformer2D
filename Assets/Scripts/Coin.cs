fa3610e30000b4579d0971c1d1ad482573da38bd
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-08-22 00:57:49 +0300

    Update Coin.cs

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 
-public class Coin : MonoBehaviour
+public class Coin : Item
 {
     [SerializeField] private int _rotationSpeedY = 100;
     private int _rotationSpeedX = 0;
     private int _rotationSpeedZ = 0;
 
+    private int _coinValue = 1;
+    
     private void Update()
     {
         transform.Rotate(_rotationSpeedX, _rotationSpeedY * Time.deltaTime, _rotationSpeedZ);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.gameObject.TryGetComponent(out Player _)))
+        {
+            FindObjectOfType<Wallet>().AddCoins(_coinValue);
+            Destroy(gameObject);
+        }
     }
 }
