f9ca6f368219684c132f871583b3f8ba48d6c398
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-08-22 00:58:15 +0300

    Update Spawner.cs

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,13 +8,43 @@ public class Spawner : MonoBehaviour
     private void Start()
     {
         SpawnCoins();
+        SpawnAidKit();
     }
 
     private void SpawnCoins()
     {
         foreach (SpawnPoint spawnPoint in _spawnPoints)
         {
-            Instantiate(spawnPoint.CoinPrefab, spawnPoint.Position.position, Quaternion.identity);
+            if (spawnPoint.ItemPrefab is Coin) 
+            Instantiate(spawnPoint.ItemPrefab, spawnPoint.Position.position, Quaternion.identity);          
+        }
+    }
+
+    private void SpawnAidKit()
+    {
+        List<int> availableIndexes = new List<int>(); 
+        
+        for (int i = 0; i < _spawnPoints.Count; i++)
+        {
+            if (_spawnPoints[i].ItemPrefab is AidKit)
+            availableIndexes.Add(i);
+        }
+
+        int minCount = 1;
+        int minRange = 0;
+        int maxRange = 2;
+
+        int kitsToSpawn = Random.Range(minCount, availableIndexes.Count);
+        
+        for (int i = 0; i < kitsToSpawn; i++)
+        {
+            int randomIndex = Random.Range(minRange, maxRange); 
+
+            int spawnPointIndex = availableIndexes[randomIndex]; 
+
+            Instantiate(_spawnPoints[spawnPointIndex].ItemPrefab, _spawnPoints[spawnPointIndex].Position.position, Quaternion.identity);
+
+            availableIndexes.RemoveAt(randomIndex); 
         }
     }
 }
