b4bb9c1d7c0bfe70ff9baab7d5b5baadf7069afe
Astor-S <154661724+Astor-S@users.noreply.github.com>
2024-08-22 00:58:13 +0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,37 @@ public class Player : MonoBehaviour
     [SerializeField] private GroundDetector _groundDetector;
     [SerializeField] private Mover _mover;
     [SerializeField] private Animator _animator;
-    
+    [SerializeField] private Health _health;
+    [SerializeField] private float _damage = 25f;
+
+    public Health Health => _health;
+
+    private void Update()
+    {
+        CheckDeath();
+    }
+
     private void FixedUpdate()
     {
         _mover.Move(_inputReader.Direction);
 
         if (_inputReader.GetIsJump() && _groundDetector.IsGround)
-        {
             _mover.Jump();
-        }
 
         Flip();
 
         _animator.SetFloat("Speed", Mathf.Abs(_inputReader.Direction));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.gameObject.TryGetComponent(out Enemy enemy)))
+        Attack(enemy);
+    }
+
+    public bool IsAlive()
+    {
+        return _health.CurrentHealth > 0;
     }
 
     private void Flip()
@@ -27,12 +45,21 @@ public class Player : MonoBehaviour
         float scaleY = 4;
 
         if (_inputReader.Direction > 0)
-        {
             transform.localScale = new Vector2(scaleX, scaleY);
-        }
         else if (_inputReader.Direction < 0)
-        {
-            transform.localScale = new Vector2(-scaleX, scaleY);
-        }
+        transform.localScale = new Vector2(-scaleX, scaleY);
+    }
+
+    private void Attack(Enemy enemy)
+    {
+        if (enemy.Health != null)
+            enemy.Health.TakeDamage(_damage);
+        
+    }
+
+    private void CheckDeath()
+    {
+        if (IsAlive() == false)
+           Destroy(gameObject);
     }
 }
